691e89fac3a43687256a7a81ea0594dd976864d8
YAKKUN Yasuyoshi IWAICHI <yakkun@yakkun.dev>
2019-12-19 15:11:35 +0900

    Put diff-highlight on PATH with macOS

--- osx-config.sh
+++ osx-config.sh
@@ -58,3 +58,6 @@ defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 defaults write com.apple.Safari ShowStatusBar -bool true
+
+### diff-highlight on PATH
+sudo ln -sf /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/
